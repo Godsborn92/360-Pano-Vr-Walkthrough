de0e11658c216f7e42856d4402a649ee90436010
Godsborn92 <33069789+Godsborn92@users.noreply.github.com>
2018-01-03 15:12:53 -0800

    test

--- js/index.js
+++ js/index.js
@@ -64,7 +64,7 @@ var config = {
                         },
 
                         events: {
-                            onClick: ["action-loadscene-2", "action-camera-2"]
+                            onClick: ["action-loadscene-2", "action-camera-1"]
                         }
                     },
 
@@ -99,7 +99,7 @@ var config = {
                         },
 
                         events: {
-                            onClick: ["action-loadscene-1", "action-camera-2"]
+                            onClick: ["action-loadscene-2", "action-camera-1"]
                         }
                     }
                 ]
