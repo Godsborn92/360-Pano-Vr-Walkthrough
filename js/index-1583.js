fb5511e8ed4cd6a8d29ab552f84fe716d4cd36c1
Godsborn92 <33069789+Godsborn92@users.noreply.github.com>
2018-01-25 16:09:05 -0800

    Remove VR and Image update
    update images add two slides to index
    remove vr button for now

--- js/index-1583.js
+++ js/index-1583.js
@@ -2,15 +2,15 @@
 // Create a viewer
 var viewer = new FORGE.Viewer("container", "config-1583.json");
 
-document.getElementById("vr").onclick = function() {
-    var el = document.getElementById("vr").firstChild;
-    if (el.data == "Exit VR") {
-        el.data = "Enter VR";
-    } else {
-        el.data = "Exit VR";
-    }
-    viewer.renderer.toggleVR();
-};
+//document.getElementById("vr").onclick = function() {
+//    var el = document.getElementById("vr").firstChild;
+//    if (el.data == "Exit VR") {
+//        el.data = "Enter VR";
+//    } else {
+//        el.data = "Exit VR";
+//    }
+//    viewer.renderer.toggleVR();
+//};
 
 document.getElementById("home").onclick = function(){
 	window.location.assign("index.html");
