588053e2db7d85eed3f11dffb1f7f264b1a9e8de
Godsborn92 <33069789+Godsborn92@users.noreply.github.com>
2018-01-29 12:05:44 -0800

    update
    add gyro control to application via toggle button

--- js/index-1583.js
+++
@@ -1,5 +0,0 @@
-//Add Home Button to Main Index from Viewer
-document.getElementById("home").onclick = function(){
-	window.location.assign("index.html");
-};
-
--- js/index.js
+++ js/index.js
@@ -1,17 +1,4 @@
-
-// Create a viewer
-var viewer = new FORGE.Viewer("container", "config.json");
-
-document.getElementById("vr").onclick = function() {
-    var el = document.getElementById("vr").firstChild;
-    if (el.data == "Exit VR") {
-        el.data = "Enter VR";
-    } else {
-        el.data = "Exit VR";
-    }
-    viewer.renderer.toggleVR();
-};
-
+//Add Home Button to Main Index from Viewer
 document.getElementById("home").onclick = function(){
 	window.location.assign("index.html");
 };
