22a1e00f9a9d1d842db0df3e4f4dcb144d58e61d
Godsborn92 <33069789+Godsborn92@users.noreply.github.com>
2018-02-22 17:04:23 -0800

    1583 now the 1602
    Update to the 1583 has been renamed to the 1602 and new files added

--- js/index.js
+++ js/index.js
@@ -3,3 +3,12 @@ document.getElementById("home").onclick = function(){
 	window.location.assign("index.html");
 };
 
+/*Create a location Floor plan view
+var scene = window.document.documentURI;
+
+if(scene == "http://127.0.0.1:8887/360-1602.html#first-scene&uid=scene-1"){
+	var redDot = document.getElementById('redDotEntry');
+	console.log(redDot);
+	redDot.hidden = false;
+}
+*/
